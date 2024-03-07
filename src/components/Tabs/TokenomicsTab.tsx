0fd0c416bd7f76ef7babd3fe31af754e1f67df93
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2024-03-07 09:48:38 +0530

    tokenomics (doughnut chart left)

---
+++ src/components/Tabs/TokenomicsTab.tsx
@@ -0,0 +1,23 @@
+import Card from '../ui/card'
+const TokenomicsTab = () => {
+  return (
+    <Card className="mt-5 space-y-5">
+      <p className="font-semibold text-2xl">Tokenomics</p>
+      <div className="space-y-2">
+        <p className="font-semibold text-xl">Initial Distribution</p>
+        <p className="font-medium text-[#3E424A]">
+          Lorem ipsum dolor sit amet consectetur. Cras aliquet tristique ornare
+          vestibulum nunc dignissim vel consequat. Leo etiam nascetur bibendum
+          amet enim sit eget leo amet. At metus orci augue fusce eleifend lectus
+          eu fusce adipiscing. Volutpat ultrices nibh sodales massa habitasse
+          urna felis augue. Gravida aliquam fermentum augue eu. Imperdiet
+          bibendum amet aliquam donec. Eget justo dui metus odio rutrum. Vel
+          ipsum eget in at curabitur sem posuere facilisis vitae. Sed lorem sit
+          mauris id eget arcu ut. Vulputate ipsum aliquet odio nisi eu ac risus.
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default TokenomicsTab
